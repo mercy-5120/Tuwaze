da250d25990854edf3927901fb15ef1b72865777
mercy-5120 <mgashau18@gmail.com>
2026-02-05 11:40:39 +0300

    Created the login, signup and home page

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 const mysql = require("mysql2");
 const express = require("express");
+const {getGenderCount} = require("./utilityFunctions")
 const app = express();
 
-const connection = mysql.createConnection({
-    host: "localhost",
-    database: "tuwaze",
-    user:   "root",
-    password: "sonnie2006.",
-    port: 3306
-}
-)
 
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  database: "tuwaze",
+  user: "root",
+  password: "sonnie2006.",
+  port: 3306,
+});
+
+app.get("/", (req, res)=>{
+  res.render("home.ejs")
+});
+
+app.get("/signup", (req, res)=>{
+  res.render("signup.ejs")
+});
+
+app.get("/login", (req, res)=>{
+  res.render("login.ejs")
+});
 
 app.get("/dashboard", (req, res) => {
-    connection.query("SELECT * FROM users", (dbError, queryResult)=>{
-    if(dbError){
-        console.log("DB Error Occured: " +dbError.message);}
-        else{
-            console.log(getGenderCount(queryResult));
-            res.render("dashboard.ejs", {allUsers: queryResult, maleFemaleCount: getGenderCount(queryResult)});
-        }    
+  connection.query("SELECT * FROM users;", (dbError, queryResult) => {
+    if (dbError) {
+      console.log("DB error occurred: " + dbError.message);
+      res.status(500).send("Database error");
+    } else {
+      const genderCount = getGenderCount(queryResult);
+      console.log(genderCount);
+      
+      // Prepare data for the chart
+      const chartData = {
+        labels: ['Male', 'Female'],
+        datasets: [{
+          data: [genderCount.male, genderCount.female],
+          backgroundColor: ['#0358B6', '#F46300'],
+          hoverBackgroundColor: ['#44DE28', '#D60000']
+        }]
+      };
+      
+      res.render("dashboard.ejs", {
+        allUsers: queryResult, 
+        maleFemaleCount: genderCount,
+        chartData: JSON.stringify(chartData)
+      });
+    }
+  });
 });
+
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
-
-app.listen(3000)
-
-
-function getGenderCount(users){
-    return users.reduce(
-        (count, user) => {
-            if (user.gender === "Male") count.male += 1;
-            if (user.gender === "Female") count.female += 1;
-            return count;
-        }, {male: 0, female: 0}
-    );
-}
---
+++ utilityFunctions.js
@@ -0,0 +1,13 @@
+function getGenderCount(users) {
+  return users.reduce(
+    (count, user) => {
+      if (user.gender === "Male") count.male += 1;
+      if (user.gender === "Female") count.female += 1;
+      return count;
+    },
+    { male: 0, female: 0 },
+  );
+}
+
+
+module.exports = {getGenderCount}
