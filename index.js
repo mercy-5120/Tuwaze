850fa48381d7042e9d2b5dea76db03581b7ccead
mercy-5120 <mgashau18@gmail.com>
2026-02-10 12:47:24 +0300

    Added authentication

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+//1.Import and configure package modulesat the top of the file
 const mysql = require("mysql2");
 const express = require("express");
-const {getGenderCount} = require("./utilityFunctions")
+const session = require("express-session");
+const { getGenderCount } = require("./utilityFunctions");
 const app = express();
-
-
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -12,35 +12,83 @@ const connection = mysql.createConnection({
   password: "sonnie2006.",
   port: 3306,
 });
-
-app.get("/", (req, res)=>{
-  res.render("home.ejs")
+//2. Register middleware functions to handle incoming requests and responses
+app.use(
+  session({
+    secret:"encryptionKey",
+    resave: false,
+    saveUninitialized: true,
+    options: {secure: true, expires: new Date(Date.now() +60 * 60 * 1000)} //expires in 1 hour
+    
+  }),
+);
+const isLoggedIn = true;
+const privateRoutes = ["/dashboard", "/profile"];
+app.use((req, res, next) => {
+  console.log("Middleware function executed!");
+  if (isLoggedIn || !privateRoutes.includes(req.path)) {
+    next();
+  } else {
+    res.status(401).send("Unauthorized!!!");
+  }
+});
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies (form data)
+//3. Register routes/pages/endpoint handlers
+app.get("/", (req, res) => {
+  res.render("home.ejs");
 });
 
-app.get("/signup", (req, res)=>{
-  res.render("signup.ejs")
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
 });
 
-app.post("/signup",express.urlencoded({extended: true}), (req, res) => {//encodes the form data and makes it available in req.body
+app.post("/signup", (req, res) => {
+  //encodes the form data and makes it available in req.body
   console.log(req.body);
   const insertStatement = `INSERT INTO users (full_name, phone_number, email, gender, password_hash, role, ward, is_anonymous_allowed) VALUES
 ('${req.body.fullname}', '${req.body.phone}', '${req.body.email}', '${req.body.gender}', '${req.body.password}', 'citizen', '${req.body.location}', TRUE)`;
 
-connection.query(insertStatement, (insertError)=>{
-  if(insertError){
-    res.status(500).send("Server Error!!!");
-  }else{
-    res.redirect("/login");
-  }
-})
+  connection.query(insertStatement, (insertError) => {
+    if (insertError) {
+      res.status(500).send("Server Error!!!");
+    } else {
+      res.redirect("/login");
+    }
+  });
 
   //res.send("Data received!!!");
   //console.log(insertStatement);
-  
 });
 
-app.get("/login", (req, res)=>{
-  res.render("login.ejs")
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.post("/auth", (req, res) => {
+  console.log(req.body); //req.body.pass -- password in db
+  connection.query(
+    `SELECT * FROM users WHERE email = '${req.body.email}'`,
+    (dbError, queryResult) => {
+      if (dbError) {
+        console.log("DB error occurred: " + dbError.message);
+        res.status(500).send("Server error");
+      } else {
+        console.log(queryResult);
+        if (queryResult.length > 0) {
+        if (req.body.password === queryResult[0].password_hash) {
+          //passwords match, user is authenticated
+          req.session.user = queryResult[0]; //store user info in session manager
+          res.redirect("/dashboard");
+          //password is wrong, authentication failed
+        } else {
+          res.send("Invalid email or password");
+        }}
+        else{
+          res.send("Invalid email or password");
+        }
+      }
+    },
+  );
 });
 
 app.get("/dashboard", (req, res) => {
@@ -51,27 +99,38 @@ app.get("/dashboard", (req, res) => {
     } else {
       const genderCount = getGenderCount(queryResult);
       console.log(genderCount);
-      
+
       // Prepare data for the chart
       const chartData = {
-        labels: ['Male', 'Female'],
-        datasets: [{
-          data: [genderCount.male, genderCount.female],
-          backgroundColor: ['#0358B6', '#F46300'],
-          hoverBackgroundColor: ['#44DE28', '#D60000']
-        }]
+        labels: ["Male", "Female", "Other", "Prefer not to say"],
+        datasets: [
+          {
+            data: [
+              genderCount.male,
+              genderCount.female,
+              genderCount.other,
+              genderCount.prefer_not_to_say,
+            ],
+            backgroundColor: ["#0358B6", "#F46300", "#8E44AD", "#7F8C8D"],
+            hoverBackgroundColor: ["#198ed6", "#d69264", "#cb89e1", "#95A5A6"],
+          },
+        ],
       };
-      
+
       res.render("dashboard.ejs", {
-        allUsers: queryResult, 
+        allUsers: queryResult,
         maleFemaleCount: genderCount,
-        chartData: JSON.stringify(chartData)
+        chartData: JSON.stringify(chartData),
       });
     }
   });
 });
+//404 Handler - This should be the last route handler to catch all unmatched routes
+app.use((req, res) => {
+  res.status(404).render("404.ejs");
+});
 
-
+//4. Start the server and listen for incoming requests
 app.listen(3000, () => {
   console.log("Server running on port 3000");
 });
--- utilityFunctions.js
+++ utilityFunctions.js
@@ -3,9 +3,11 @@ function getGenderCount(users) {
     (count, user) => {
       if (user.gender === "Male") count.male += 1;
       if (user.gender === "Female") count.female += 1;
+      if (user.gender === "Other") count.other +=1;
+      if (user.gender === "Prefer not to say") count.prefer_not_to_say +=1;
       return count;
     },
-    { male: 0, female: 0 },
+    { male: 0, female: 0, other: 0, prefer_not_to_say: 0 },
   );
 }
 
